f4f7e1e7d4632d56a135674d96c3c3b95c2b183c
alumno <alumno@pc-candelap.uacj.mx>
2024-09-27 10:41:29 -0600

    [NUEVACITA]Agregar la funcion de craear una nueva cita

--- boceto_01/boceto_01/Controlador_de_vistas/CitasUsuario.swift
+++ boceto_01/boceto_01/Controlador_de_vistas/CitasUsuario.swift
@@ -7,7 +7,20 @@
 
 import Foundation
 import UIKit
-class ControladorCitasUsuario: UIViewController {
+class ControladorAgregarCitasUsuario: UIViewController {
+    
+    var quien_lo_dice: String = ""
+    var que_es_lo_que_dice: String = ""
+    
+    var cita_creada: cita? = nil
+    
+    @IBOutlet weak var Nombre_de_quien_lo_dijo: UITextField!
+    @IBOutlet weak var Que_es_lo_que_dijo: UITextField!
+    
+    @IBAction func Agregar_cita_nueva(_ sender: UIButton) {
+        cita_creada = cita(quien_lo_dijo:Nombre_de_quien_lo_dijo.text!,
+                           que_dijo: Que_es_lo_que_dijo.text!)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
--- boceto_01/boceto_01/ViewController.swift
+++ boceto_01/boceto_01/ViewController.swift
@@ -12,17 +12,28 @@ class ViewController: UIViewController {
     var citaParEnviar: cita = cita(quien_lo_dijo: "Pato", que_dijo: "pio pio")
     var citas_disponibles : Generador_De_Citas = Generador_De_Citas()
 
+    @IBOutlet weak var Labelcito: UILabel!
+    
     override func viewDidLoad() {
         citas_disponibles.generar_citas_falsas()
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        actualizar_cantidad()
     }
+    func actualizar_cantidad(){
+        Labelcito.text = String(citas_disponibles.citas_creadas.count)
+    }
+    
     @IBSegueAction func al_abrir_la_pantalla(_ coder: NSCoder) -> ControladorCitas? {
         return ControladorCitas(cita_para_citar: citas_disponibles.obtener_cita_aleatoria(), coder: coder)
     }
-
-
-
-
+    
+    @IBAction func volver_al_inicio(segue: UIStoryboardSegue){
+        if let pantalla_agregar_citas = segue.source as?
+                ControladorAgregarCitasUsuario{
+             citas_disponibles.agregar_cita(pantalla_agregar_citas.cita_creada!)
+         }
+        actualizar_cantidad()
+    }
 }
 
