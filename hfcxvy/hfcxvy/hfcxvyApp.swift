d5aa86c7b084d6ef2a11cc0b79958a573b7156d8
alumno <alumno@DESKTOP-GUMACDG.uacj.mx>
2024-11-06 09:48:15 -0700

    NuevoProyecto

---
+++ hfcxvy/hfcxvy/hfcxvyApp.swift
@@ -0,0 +1,17 @@
+//
+//  hfcxvyApp.swift
+//  hfcxvy
+//
+//  Created by alumno on 11/6/24.
+//
+
+import SwiftUI
+
+@main
+struct hfcxvyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
